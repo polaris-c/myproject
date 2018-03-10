a5214812e5ff269d1c5de09d206c530630c096b3
ZSY <1169846461@qq.com>
2018-03-10 18:32:05 +0800

    zsy master create backend api

--- backend/models.py
+++ backend/models.py
@@ -9,7 +9,7 @@ from django.db import models
 class Book(models.Model):
     book_name = models.CharField(max_length=64)
     add_time = models.DateTimeField(auto_now_add=True)
-    book_price = models.IntegerField()
+    # book_price = models.IntegerField()
 
     def  __str__(self):
         return self.book_name
